8506c206fe312b39666518631b0928cd864bbc40
liveligang <88247986+liveligang@users.noreply.github.com>
2021-08-07 01:14:11 +0700

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+function lihat_menu() {
+    document.getElementById("lihat-menu").style.display = "none";
+    document.getElementById("tutup-menu").style.display = "block";
+    document.getElementById("menu").style.display = "block";
+}
+function tutup_menu() {
+    document.getElementById("lihat-menu").style.display = "block";
+    document.getElementById("tutup-menu").style.display = "none";
+    document.getElementById("menu").style.display = "none";
+}
